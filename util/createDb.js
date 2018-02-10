6910a3217c2ae58733067350aa9e7c5b302181d1
yoshuro <yoshuro@hotmail.com>
2018-02-11 00:33:39 +0100

    Move db script outside of src

--- util/createDb.js
+++ util/createDb.js
@@ -1,7 +1,7 @@
 const exec = require('child_process').exec
 const path = require('path')
 
-const dbPath = path.join(__dirname, '..', '..')
+const dbPath = path.join(__dirname, '..')
 
 const cmd = `docker run -d -p 28015:28015 -p 8090:8080 -v ${dbPath}:/data --name tanglewood rethinkdb`
 
