16708f9aa87510229ef595214927b9858e1e1dfb
Yoshuro <yoshuro@hotmail.com>
2018-05-23 17:55:04 +0200

    Add another test to adding to inventory action

--- test/user/addToInventory.test.js
+++ test/user/addToInventory.test.js
@@ -1,19 +1,36 @@
 const makeAddToInventory = require('services/user/actions/addToInventory')
 
-const getUserById = async () => ({
+const user = {
   inventory: {
     ingredients: {},
     items: [],
   },
-})
-const updateUserById = (_, x) => x
+}
+
+const getUserById = async () => user
+const updateUserById = (_, x) => {
+  const newInventory = Object.assign(
+    user.inventory,
+    x.inventory
+  )
+
+  user.inventory = newInventory
+  return user
+}
 
 const addToInventory = makeAddToInventory({
   getUserById,
   updateUserById,
 })
 
-describe('addToInventory', () => {
+describe.only('addToInventory', () => {
+  beforeEach(() => {
+    user.inventory = {
+      ingredients: {},
+      items: [],
+    }
+  })
+
   test('Adds items to inventory', async () => {
     const ingredients = {
       'Cherry Petals': 3,
@@ -24,5 +41,28 @@ describe('addToInventory', () => {
     })
 
     expect(updated.inventory.ingredients).toEqual(ingredients)
+  })
+
+  test('Correctly add ingredient quantity', async () => {
+    const batch1 = {
+      'Sword Pearls': 3,
+    }
+
+    const batch2 = {
+      'Sword Pearls': 5,
+    }
+
+    await addToInventory('test', {
+      ingredients: batch1,
+    })
+
+    const updated = await addToInventory('test', {
+      ingredients: batch2,
+    })
+
+    const ingredients = updated.inventory.ingredients
+    const pearls = ingredients['Sword Pearls']
+
+    expect(pearls).toBe(8)
   })
 })
