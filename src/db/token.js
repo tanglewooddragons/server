c4d06532310ff1b14f306d7fcddf024a1e25b5f2
Yoshuro <yoshuro@hotmail.com>
2018-06-08 13:55:45 +0200

    Improve token logging

--- src/db/token.js
+++ src/db/token.js
@@ -1,60 +1,79 @@
+const log = require('util/log')
+
 const Token = require('./models/token')
-const log = require('../util/log')
 
 async function getToken(token) {
   try {
-    log.debug(`Getting token ${token}..`)
     const tokens = await Token.filter({ token }).run()
-    log.debug(`Got token ${token}`)
     return tokens
-  } catch (err) {
-    log.error(`Error getting tokens: ${err}`)
+  } catch (error) {
+    log.error({
+      action: 'get-token',
+      status: 'failed',
+      error,
+    })
+
     return null
   }
 }
 
 async function saveToken(context) {
   try {
-    log.debug(`Saving token for ${context.userId}`)
     const token = new Token(context)
     await token.save()
-    log.debug(`Token saved for ${context.userId}`)
     return token
-  } catch (err) {
-    log.error(`Error saving token: ${err}`)
+  } catch (error) {
+    log.error({
+      action: 'save-token',
+      status: 'failed',
+      error,
+    })
+
     return null
   }
 }
 
 async function removeToken(token) {
   try {
-    log.debug(`Removing token: ${token}`)
     const tokenEntry = await Token.filter({ token }).run()
     await tokenEntry[0].delete()
     return true
-  } catch (err) {
-    log.error(`Error removing token: ${err}`)
+  } catch (error) {
+    log.error({
+      action: 'remove-token',
+      status: 'failed',
+      error,
+    })
+
     return null
   }
 }
 
 async function removeAllTokens(userId) {
   try {
-    log.debug(`Removing all tokens for ${userId}`)
     const tokenEntries = await Token.filter({ userId }).run()
 
     if (!tokenEntries.length) return true
     await tokenEntries.forEach(async t => t.delete())
     return true
-  } catch (err) {
-    log.error(`Error removing tokens: ${err}`)
+  } catch (error) {
+    log.error({
+      action: 'remove-all-tokens',
+      status: 'failed',
+      error,
+    })
+
     return null
   }
 }
 
 async function removeExpiredTokens() {
+  log.info({
+    action: 'remove-expired-tokens',
+    status: 'pending',
+  })
+
   try {
-    log.debug('Removing expired tokens..')
     const expiredTokens = await Token
       .filter(token => token.expires >= Date.now())
       .run()
@@ -62,9 +81,20 @@ async function removeExpiredTokens() {
     if (!expiredTokens) return true
 
     expiredTokens.forEach(expiredToken => expiredToken.delete())
+
+    log.info({
+      action: 'remove-expired-tokens',
+      status: 'success',
+    })
+
     return true
-  } catch (err) {
-    log.error(`Error removing expired tokens: ${err}`)
+  } catch (error) {
+    log.error({
+      action: 'remove-expired-tokens',
+      status: 'failed',
+      error,
+    })
+
     return null
   }
 }
