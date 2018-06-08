9e2cd106eab8f164bea6618c1b837b6ab6e6856b
yoshuro <yoshuro@hotmail.com>
2018-06-08 11:26:25 +0200

    Change fake timers to real delay in scheduler test

--- test/unit/scheduler/scheduleAction.test.js
+++ test/unit/scheduler/scheduleAction.test.js
@@ -1,11 +1,14 @@
-jest.useFakeTimers()
-
+// jest.useFakeTimers()
 const schedule = require('node-schedule')
 
 const {
   scheduleAction,
   registerHandler,
 } = require('services/scheduler')
+
+const sleep = async ms => new Promise((resolve) => {
+  setTimeout(resolve, ms)
+})
 
 describe('scheduleAction', () => {
   test('Fails to schedule with missing data', async () => {
@@ -30,7 +33,7 @@ describe('scheduleAction', () => {
     const options = {
       scheduledBy: 'jest',
       type: 'schedule-test',
-      scheduledFor: Date.now() + 1000,
+      scheduledFor: Date.now() + 500,
     }
 
     const resolve = async (job) => {
@@ -42,14 +45,15 @@ describe('scheduleAction', () => {
     await scheduleAction(options)
     const registeredJobAmount = Object.keys(schedule.scheduledJobs).length
     expect(registeredJobAmount).toBe(1)
-    jest.runOnlyPendingTimers()
+    // jest.runOnlyPendingTimers()
+    await sleep(500)
   })
 
   test('Calls the resolve function on scheduled time', async () => {
     const options = {
       scheduledBy: 'jest',
       type: 'call-test',
-      scheduledFor: Date.now() + 1000,
+      scheduledFor: Date.now() + 500,
     }
 
     const resolve = jest.fn()
@@ -58,7 +62,8 @@ describe('scheduleAction', () => {
     await scheduleAction(options)
 
     expect(resolve).not.toBeCalled()
-    jest.runOnlyPendingTimers()
+    // jest.runOnlyPendingTimers()
+    await sleep(500)
     expect(resolve).toBeCalled()
     expect(resolve).toHaveBeenCalledTimes(1)
   })
