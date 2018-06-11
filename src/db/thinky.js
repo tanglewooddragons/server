95ba85a458eb5fe7ad9a9d590756f024072d3f27
Yoshuro <yoshuro@hotmail.com>
2018-06-11 23:53:48 +0200

    Remove initial password, container does not support it anyway

--- src/db/thinky.js
+++ src/db/thinky.js
@@ -4,8 +4,6 @@ const config = {
   host: 'localhost',
   port: 28015,
   db: process.env.DB_DATABASE,
-  user: 'admin',
-  password: process.env.DB_PASSWORD,
 }
 
 const thinky = initThinky(config)
--- util/createDb.js
+++ util/createDb.js
@@ -1,8 +1,16 @@
+require('dotenv').load()
+
 const exec = require('child_process').exec
 const path = require('path')
 
 const dbPath = path.join(__dirname, '..')
 
-const cmd = `docker run -d -p 28015:28015 -p 8090:8080 -v ${dbPath}:/data --name tanglewood rethinkdb`
+const dbName = process.env.DB_DATABASE
+
+const cmd = `docker run -d \
+  -p 28015:28015 -p 8090:8080 \
+  -v ${dbPath}:/data \
+  --name ${dbName} \
+  rethinkdb`
 
 exec(cmd)
