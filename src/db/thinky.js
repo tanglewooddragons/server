92254032a13efa3c3967769dca8a63113b69bf29
Yoshuro <yoshuro@hotmail.com>
2018-06-11 23:08:44 +0200

    Add CircleCi config

--- src/db/thinky.js
+++ src/db/thinky.js
@@ -3,7 +3,8 @@ const initThinky = require('thinky')
 const config = {
   host: 'localhost',
   port: 28015,
-  db: 'tanglewood',
+  db: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
 }
 
 const thinky = initThinky(config)
